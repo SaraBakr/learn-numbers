6c9e4506c749bfbaaa352e956cd387fa920283bb
sara <Sara.Bakr.Ahmad@gmail.com>
2018-08-27 11:56:06 +0300

    INITIAL COMMIT

---
+++ app/src/main/java/com/example/sara/learnnumbers/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.sara.learnnumbers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button next;
+    Button previous;
+    TextView tv;
+    int newNumber;
+    int[] numberImages;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.image);
+        next = findViewById(R.id.next);
+        previous = findViewById(R.id.prev);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        }
+
+
+    }
+
+
---
+++ app/src/main/java/com/example/sara/learnnumbers/Numbers.java
@@ -0,0 +1,10 @@
+package com.example.sara.learnnumbers;
+
+import android.media.MediaPlayer;
+
+public class Numbers {
+    public MediaPlayer voices[]={};
+
+
+
+}
