bb202fd6d990d994757a05ad40903bef7d75ccac
dalirafik95@gmail.com <daniellar>
2018-08-27 12:51:06 +0300

    added comment

--- app/src/main/java/com/example/sara/learnnumbers/MainActivity.java
+++ app/src/main/java/com/example/sara/learnnumbers/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //hello sara
+                // hhhhhhhhhhhhhhhhhhhhh
             }
         });
 
